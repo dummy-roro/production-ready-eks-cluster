a1255f87f2d7f9eccfc1d15a967287cfb29fba47
Dummy Robot <myatsoeaumg@hotmail.com>
2025-07-08 11:25:33 +0630

    Update eks.tf

--- module/eks.tf
+++ module/eks.tf
@@ -1,111 +1,73 @@
-resource "aws_eks_cluster" "eks" {
+locals {
+  name_prefix = "${var.cluster_name}-${var.env}"
+}
 
-  count    = var.is-eks-cluster-enabled == true ? 1 : 0
-  name     = var.cluster-name
-  role_arn = aws_iam_role.eks-cluster-role[count.index].arn
-  version  = var.cluster-version
+resource "aws_eks_cluster" "this" {
+  name     = local.name_prefix
+  role_arn = aws_iam_role.cluster.arn
+  version  = var.cluster_version
 
   vpc_config {
-    subnet_ids              = [aws_subnet.private-subnet[0].id, aws_subnet.private-subnet[1].id, aws_subnet.private-subnet[2].id]
-    endpoint_private_access = var.endpoint-private-access
-    endpoint_public_access  = var.endpoint-public-access
-    security_group_ids      = [aws_security_group.eks-cluster-sg.id]
+    subnet_ids              = values(aws_subnet.private)[*].id
+    endpoint_private_access = var.endpoint_private_access
+    endpoint_public_access  = var.endpoint_public_access
+    security_group_ids      = [aws_security_group.cluster.id]
   }
 
-
-  access_config {
-    authentication_mode                         = "CONFIG_MAP"
-    bootstrap_cluster_creator_admin_permissions = true
-  }
-
-  tags = {
-    Name = var.cluster-name
-    Env  = var.env
-  }
-}
-
-# OIDC Provider
-resource "aws_iam_openid_connect_provider" "eks-oidc" {
-  client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = [data.tls_certificate.eks-certificate.certificates[0].sha1_fingerprint]
-  url             = data.tls_certificate.eks-certificate.url
-}
-
-
-# AddOns for EKS Cluster
-resource "aws_eks_addon" "eks-addons" {
-  for_each      = { for idx, addon in var.addons : idx => addon }
-  cluster_name  = aws_eks_cluster.eks[0].name
-  addon_name    = each.value.name
-  addon_version = each.value.version
+  tags = var.tags
 
   depends_on = [
-    aws_eks_node_group.ondemand-node,
-    aws_eks_node_group.spot-node
+    aws_iam_role_policy_attachment.cluster_policy
   ]
 }
 
-# NodeGroups
-resource "aws_eks_node_group" "ondemand-node" {
-  cluster_name    = aws_eks_cluster.eks[0].name
-  node_group_name = "${var.cluster-name}-on-demand-nodes"
+# OIDC Provider for IAM Roles for Service Accounts (IRSA)
+resource "aws_iam_openid_connect_provider" "this" {
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [] # AWS Provider automatically handles this now
+  url             = aws_eks_cluster.this.identity[0].oidc[0].issuer
 
-  node_role_arn = aws_iam_role.eks-nodegroup-role[0].arn
-
-  scaling_config {
-    desired_size = var.desired_capacity_on_demand
-    min_size     = var.min_capacity_on_demand
-    max_size     = var.max_capacity_on_demand
-  }
-
-
-  subnet_ids = [aws_subnet.private-subnet[0].id, aws_subnet.private-subnet[1].id, aws_subnet.private-subnet[2].id]
-
-  instance_types = var.ondemand_instance_types
-  capacity_type  = "ON_DEMAND"
-  labels = {
-    type = "ondemand"
-  }
-
-  update_config {
-    max_unavailable = 1
-  }
-  tags = {
-    "Name" = "${var.cluster-name}-ondemand-nodes"
-  }
-
-  depends_on = [aws_eks_cluster.eks]
+  tags = var.tags
 }
 
-resource "aws_eks_node_group" "spot-node" {
-  cluster_name    = aws_eks_cluster.eks[0].name
-  node_group_name = "${var.cluster-name}-spot-nodes"
+# EKS Add-ons
+resource "aws_eks_addon" "this" {
+  for_each = { for addon in var.addons : addon.name => addon }
 
-  node_role_arn = aws_iam_role.eks-nodegroup-role[0].arn
+  cluster_name  = aws_eks_cluster.this.name
+  addon_name    = each.key
+  addon_version = each.value.version
+}
+
+# EKS Managed Node Groups
+resource "aws_eks_node_group" "this" {
+  for_each = var.node_groups
+
+  cluster_name    = aws_eks_cluster.this.name
+  node_group_name = "${local.name_prefix}-ng-${each.key}"
+  node_role_arn   = aws_iam_role.nodegroup.arn
+  subnet_ids      = values(aws_subnet.private)[*].id
+
+  instance_types = each.value.instance_types
+  capacity_type  = each.value.capacity_type
 
   scaling_config {
-    desired_size = var.desired_capacity_spot
-    min_size     = var.min_capacity_spot
-    max_size     = var.max_capacity_spot
+    min_size     = each.value.min_size
+    max_size     = each.value.max_size
+    desired_size = each.value.desired_size
   }
-
-
-  subnet_ids = [aws_subnet.private-subnet[0].id, aws_subnet.private-subnet[1].id, aws_subnet.private-subnet[2].id]
-
-  instance_types = var.spot_instance_types
-  capacity_type  = "SPOT"
 
   update_config {
     max_unavailable = 1
   }
-  tags = {
-    "Name" = "${var.cluster-name}-spot-nodes"
-  }
-  labels = {
-    type      = "spot"
-    lifecycle = "spot"
-  }
-  disk_size = 50
 
-  depends_on = [aws_eks_cluster.eks]
+  tags = merge(var.tags, {
+    Name = "${local.name_prefix}-ng-${each.key}"
+  })
+
+  depends_on = [
+    aws_iam_role_policy_attachment.nodegroup_worker_policy,
+    aws_iam_role_policy_attachment.nodegroup_cni_policy,
+    aws_iam_role_policy_attachment.nodegroup_ecr_readonly_policy,
+  ]
 }
