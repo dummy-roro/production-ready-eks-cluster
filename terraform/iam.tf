fda90eb82981f303a108963d06f4e05908384671
Dummy Robot <myatsoeaumg@hotmail.com>
2025-07-08 15:45:42 +0630

    Update iam.tf

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,4 +1,6 @@
+# ---------------------------------
 # IAM Role for the EKS Cluster
+# ---------------------------------
 resource "aws_iam_role" "cluster" {
   count = var.is_eks_cluster_enabled ? 1 : 0
   name  = "${var.env}-${var.cluster_name}-cluster-role"
@@ -18,7 +20,10 @@ resource "aws_iam_role_policy_attachment" "cluster_policy" {
   role       = aws_iam_role.cluster[0].name
 }
 
+
+# ---------------------------------
 # IAM Role for the EKS Node Groups
+# ---------------------------------
 resource "aws_iam_role" "node" {
   count = var.is_eks_cluster_enabled ? 1 : 0
   name  = "${var.env}-${var.cluster_name}-node-role"
@@ -50,7 +55,10 @@ resource "aws_iam_role_policy_attachment" "node_registry_policy" {
   role       = aws_iam_role.node[0].name
 }
 
+
+# ---------------------------------
 # IAM Role for the Jump Host
+# ---------------------------------
 resource "aws_iam_role" "jump_host" {
   name = "${var.env}-jump-host-role"
   assume_role_policy = jsonencode({
@@ -61,6 +69,22 @@ resource "aws_iam_role" "jump_host" {
       Principal = { Service = "ec2.amazonaws.com" }
     }]
   })
+
+  # This inline policy grants the jump host permission to describe EKS clusters
+  # which is required for the `aws eks update-kubeconfig` command.
+  inline_policy {
+    name = "eks-describe-clusters"
+    policy = jsonencode({
+      Version = "2012-10-17",
+      Statement = [
+        {
+          Action   = ["eks:DescribeCluster"],
+          Effect   = "Allow",
+          Resource = "*"
+        }
+      ]
+    })
+  }
 }
 
 resource "aws_iam_instance_profile" "jump_host" {
