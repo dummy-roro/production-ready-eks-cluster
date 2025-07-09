c92fcbd4e600acd3826f0435e9c670c10ef6943c
Dummy Robot <myatsoeaumg@hotmail.com>
2025-07-09 19:34:23 +0630

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,63 +1,61 @@
-resource "aws_vpc" "main" {
-  cidr_block           = var.vpc_cidr
+resource "aws_vpc" "this" {
+  cidr_block           = var.vpc-cidr-block
   enable_dns_support   = true
   enable_dns_hostnames = true
 
   tags = {
-    Name = var.vpc_name
+    Name = var.vpc-name
   }
 }
 
 resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main.id
+  vpc_id = aws_vpc.this.id
 
   tags = {
-    Name = var.igw_name
-  }
-}
-
-resource "aws_eip" "nat_eip" {
-  vpc = true
-
-  tags = {
-    Name = var.eip_name
-  }
-}
-
-resource "aws_nat_gateway" "nat" {
-  allocation_id = aws_eip.nat_eip.id
-  subnet_id     = aws_subnet.public[0].id
-
-  tags = {
-    Name = var.ngw_name
+    Name = var.igw-name
   }
 }
 
 resource "aws_subnet" "public" {
-  count                   = var.pub_subnet_count
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = var.pub_cidr_block[count.index]
-  availability_zone       = var.pub_azs[count.index]
-  map_public_ip_on_launch = true
+  count                    = var.pub-subnet-count
+  vpc_id                   = aws_vpc.this.id
+  cidr_block               = var.pub-cidr-block[count.index]
+  availability_zone        = var.pub-availability-zone[count.index]
+  map_public_ip_on_launch  = true
 
   tags = {
-    Name = "${var.pub_sub_name}-${count.index + 1}"
+    Name = "${var.pub-sub-name}-${count.index}"
   }
 }
 
 resource "aws_subnet" "private" {
-  count             = var.pri_subnet_count
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = var.pri_cidr_block[count.index]
-  availability_zone = var.pri_azs[count.index]
+  count             = var.pri-subnet-count
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = var.pri-cidr-block[count.index]
+  availability_zone = var.pri-availability-zone[count.index]
 
   tags = {
-    Name = "${var.pri_sub_name}-${count.index + 1}"
+    Name = "${var.pri-sub-name}-${count.index}"
+  }
+}
+
+resource "aws_eip" "ngw" {
+  tags = {
+    Name = var.eip-name
+  }
+}
+
+resource "aws_nat_gateway" "ngw" {
+  allocation_id = aws_eip.ngw.id
+  subnet_id     = aws_subnet.public[0].id
+
+  tags = {
+    Name = var.ngw-name
   }
 }
 
 resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.id
+  vpc_id = aws_vpc.this.id
 
   route {
     cidr_block = "0.0.0.0/0"
@@ -65,31 +63,31 @@ resource "aws_route_table" "public" {
   }
 
   tags = {
-    Name = var.public_rt_name
+    Name = var.public-rt-name
   }
 }
 
-resource "aws_route_table_association" "public_assoc" {
-  count          = var.pub_subnet_count
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.ngw.id
+  }
+
+  tags = {
+    Name = var.private-rt-name
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  count          = var.pub-subnet-count
   subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
 }
 
-resource "aws_route_table" "private" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat.id
-  }
-
-  tags = {
-    Name = var.private_rt_name
-  }
-}
-
-resource "aws_route_table_association" "private_assoc" {
-  count          = var.pri_subnet_count
+resource "aws_route_table_association" "private" {
+  count          = var.pri-subnet-count
   subnet_id      = aws_subnet.private[count.index].id
   route_table_id = aws_route_table.private.id
 }
