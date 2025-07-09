caaf2a6e89a8c9811f196fc4b845e466a1497143
Dummy Robot <myatsoeaumg@hotmail.com>
2025-07-09 19:35:06 +0630

    Update outputs.tf

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -1,11 +1,11 @@
 output "vpc_id" {
-  value = aws_vpc.main.id
+  value = aws_vpc.this.id
 }
 
-output "public_subnets" {
+output "public_subnet_ids" {
   value = aws_subnet.public[*].id
 }
 
-output "private_subnets" {
+output "private_subnet_ids" {
   value = aws_subnet.private[*].id
 }
