5491568c69d29b64b8439ca3d0922f62e39b4dcf
Dummy Robot <myatsoeaumg@hotmail.com>
2025-07-09 19:34:46 +0630

    Update variables.tf

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,15 +1,23 @@
-variable "vpc_cidr" {}
-variable "vpc_name" {}
-variable "igw_name" {}
-variable "eip_name" {}
-variable "ngw_name" {}
-variable "pub_subnet_count" {}
-variable "pub_cidr_block" { type = list(string) }
-variable "pub_azs" { type = list(string) }
-variable "pub_sub_name" {}
-variable "pri_subnet_count" {}
-variable "pri_cidr_block" { type = list(string) }
-variable "pri_azs" { type = list(string) }
-variable "pri_sub_name" {}
-variable "public_rt_name" {}
-variable "private_rt_name" {}
+variable "vpc-cidr-block" {}
+variable "vpc-name" {}
+variable "igw-name" {}
+variable "pub-subnet-count" {}
+variable "pub-cidr-block" {
+  type = list(string)
+}
+variable "pub-availability-zone" {
+  type = list(string)
+}
+variable "pub-sub-name" {}
+variable "pri-subnet-count" {}
+variable "pri-cidr-block" {
+  type = list(string)
+}
+variable "pri-availability-zone" {
+  type = list(string)
+}
+variable "pri-sub-name" {}
+variable "public-rt-name" {}
+variable "private-rt-name" {}
+variable "eip-name" {}
+variable "ngw-name" {}
