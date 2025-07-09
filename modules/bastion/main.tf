59954eb6da9b5c9fd38b6f976bcbdc4dc4c7ac42
Dummy Robot <myatsoeaumg@hotmail.com>
2025-07-09 19:40:16 +0630

    Update main.tf

--- modules/bastion/main.tf
+++ modules/bastion/main.tf
@@ -1,34 +1,33 @@
-data "aws_ami" "amazon_linux" {
-  most_recent = true
-  owners      = ["amazon"]
+resource "aws_iam_role" "bastion_ssm_role" {
+  name               = "bastion-ssm-role"
+  assume_role_policy = data.aws_iam_policy_document.ec2_assume.json
+}
 
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+data "aws_iam_policy_document" "ec2_assume" {
+  statement {
+    effect = "Allow"
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
   }
 }
 
-resource "aws_security_group" "bastion_sg" {
-  name   = "bastion-sg"
-  vpc_id = var.vpc_id
+resource "aws_iam_instance_profile" "bastion_profile" {
+  name = "bastion-instance-profile"
+  role = aws_iam_role.bastion_ssm_role.name
+}
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Restrict this in production!
-  }
+resource "aws_iam_role_policy_attachment" "ssm" {
+  role       = aws_iam_role.bastion_ssm_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "bastion-sg"
-  }
+resource "aws_iam_role_policy_attachment" "eks_access" {
+  role       = aws_iam_role.bastion_ssm_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
 
 resource "aws_instance" "bastion" {
@@ -38,6 +37,8 @@ resource "aws_instance" "bastion" {
   subnet_id                   = var.public_subnet_id
   associate_public_ip_address = true
   vpc_security_group_ids      = [aws_security_group.bastion_sg.id]
+  iam_instance_profile        = aws_iam_instance_profile.bastion_profile.name
+  user_data                   = file("${path.module}/userdata.sh")
 
   tags = {
     Name = "bastion-host"
