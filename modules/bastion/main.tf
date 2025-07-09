376f16b106b67a28c6b65bd4634d9724e6c7c1c0
Dummy Robot <myatsoeaumg@hotmail.com>
2025-07-09 19:35:42 +0630

    Update main.tf

--- modules/bastion/main.tf
+++ modules/bastion/main.tf
@@ -1,12 +1,22 @@
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
 resource "aws_security_group" "bastion_sg" {
-  name        = "bastion-sg"
-  vpc_id      = var.vpc_id
+  name   = "bastion-sg"
+  vpc_id = var.vpc_id
 
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"] # Restrict this in production!
   }
 
   egress {
@@ -15,22 +25,22 @@ resource "aws_security_group" "bastion_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-}
-
-resource "aws_instance" "bastion" {
-  ami                    = "ami-0c101f26f147fa7fd"
-  instance_type          = "t3.micro"
-  subnet_id              = var.public_subnet_id
-  key_name               = var.bastion_key_pair
-  vpc_security_group_ids = [aws_security_group.bastion_sg.id]
-  iam_instance_profile   = aws_iam_instance_profile.this.name
 
   tags = {
-    Name = "bastion"
+    Name = "bastion-sg"
   }
 }
 
-resource "aws_iam_instance_profile" "this" {
-  name = "bastion-instance-profile"
-  role = var.bastion_role_arn
+resource "aws_instance" "bastion" {
+  ami                         = data.aws_ami.amazon_linux.id
+  instance_type               = "t3.micro"
+  key_name                    = var.key_pair_name
+  subnet_id                   = var.public_subnet_id
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [aws_security_group.bastion_sg.id]
+
+  tags = {
+    Name = "bastion-host"
+    Env  = var.env
+  }
 }
