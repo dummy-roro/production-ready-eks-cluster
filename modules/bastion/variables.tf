12767dd50d9f22124e115e1202b5bc876a062b34
Dummy Robot <myatsoeaumg@hotmail.com>
2025-07-10 09:56:10 +0630

    Update variables.tf

--- modules/bastion/variables.tf
+++ modules/bastion/variables.tf
@@ -1,4 +1,7 @@
+variable "aws_region" {}
+variable "cluster_name" {}
+variable "cluster_version" {}
 variable "vpc_id" {}
-variable "public_subnet_id" {}
-variable "key_pair_name" {}
-variable "env" {}
+variable "public_subnet_ids" { type = list(string) }
+variable "bastion_key_pair_name" {}
+variable "bastion_instance_profile" {}
