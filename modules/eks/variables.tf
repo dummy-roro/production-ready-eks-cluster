dcde8dbc71b553e2f8ad29bdad7b22ad15a69314
Dummy Robot <myatsoeaumg@hotmail.com>
2025-07-10 09:58:24 +0630

    Update variables.tf

--- modules/eks/variables.tf
+++ modules/eks/variables.tf
@@ -1,8 +1,20 @@
+variable "env" {}
+variable "aws_region" {}
 variable "cluster_name" {}
 variable "cluster_version" {}
-variable "private_subnet_ids" {
-  type = list(string)
-}
 variable "endpoint_private_access" {}
 variable "endpoint_public_access" {}
-variable "eks_security_group_name" {}
+variable "cluster_role_arn" {}
+variable "node_group_role_arn" {}
+variable "private_subnet_ids" { type = list(string) }
+
+variable "ondemand_instance_types" { type = list(string) }
+variable "spot_instance_types" { type = list(string) }
+
+variable "desired_capacity_on_demand" {}
+variable "min_capacity_on_demand" {}
+variable "max_capacity_on_demand" {}
+
+variable "desired_capacity_spot" {}
+variable "min_capacity_spot" {}
+variable "max_capacity_spot" {}
