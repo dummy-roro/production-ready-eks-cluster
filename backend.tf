87c895038c683b4f9b8df913f022f7afbb73e1f9
Dummy Robot <myatsoeaumg@hotmail.com>
2025-07-10 10:31:05 +0630

    Delete backend.tf

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "your-terraform-state-bucket"
-    key            = "eks-cluster/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-lock"
-    encrypt        = true
-  }
-}
