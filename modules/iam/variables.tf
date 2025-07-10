eb0460011fe6ab68d2678c353e3495597f0a75ee
Dummy Robot <myatsoeaumg@hotmail.com>
2025-07-10 10:22:35 +0630

    Create variables.tf

---
+++ modules/iam/variables.tf
@@ -0,0 +1,8 @@
+variable "node_group_policy_arns" {
+  type    = list(string)
+  default = [
+    "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
+    "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly",
+    "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  ]
+}
