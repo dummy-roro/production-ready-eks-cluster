8cdcddbd507b6e4d049e03ea61b94a72d8d1d202
Dummy Robot <myatsoeaumg@hotmail.com>
2025-07-10 09:51:15 +0630

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
-output "vpc_id" {
-  value = module.vpc.vpc_id
-}
-
-output "bastion_ip" {
-  value = module.bastion.public_ip
-}
-
-output "eks_cluster_name" {
+output "cluster_name" {
   value = module.eks.cluster_name
+}
+
+output "bastion_public_ip" {
+  value = module.bastion.bastion_public_ip
+}
+
+output "eks_endpoint" {
+  value = module.eks.cluster_endpoint
 }
