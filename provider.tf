b17b4ea351c194871e63c2b9ae07b87dfb597f6f
Dummy Robot <myatsoeaumg@hotmail.com>
2025-07-10 10:32:31 +0630

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  backend "s3" {
+    bucket         = "<your-s3-bucket-name>"
+    key            = "eks-cluster/terraform.tfstate"
+    region         = var.aws_region
+    dynamodb_table = "<your-lock-table-name>"
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
