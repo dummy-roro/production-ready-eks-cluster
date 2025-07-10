27025be07ba1a05670c713b289dc8ac6be42f68f
Dummy Robot <myatsoeaumg@hotmail.com>
2025-07-10 09:58:01 +0630

    Update main.tf

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -1,11 +1,55 @@
-module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "~> 20.0"
+resource "aws_eks_cluster" "main" {
+  name     = var.cluster_name
+  role_arn = var.cluster_role_arn
+  version  = var.cluster_version
 
-  cluster_name                     = var.cluster_name
-  cluster_version                  = var.cluster_version
-  subnet_ids                       = var.private_subnet_ids
-  cluster_endpoint_private_access  = var.endpoint_private_access
-  cluster_endpoint_public_access   = var.endpoint_public_access
-  cluster_security_group_name      = var.eks_security_group_name
+  vpc_config {
+    subnet_ids              = var.private_subnet_ids
+    endpoint_private_access = var.endpoint_private_access
+    endpoint_public_access  = var.endpoint_public_access
+  }
+
+  tags = {
+    Environment = var.env
+  }
+}
+
+resource "aws_eks_node_group" "on_demand" {
+  cluster_name    = aws_eks_cluster.main.name
+  node_group_name = "${var.cluster_name}-on-demand"
+  node_role_arn   = var.node_group_role_arn
+  subnet_ids      = var.private_subnet_ids
+
+  scaling_config {
+    desired_size = var.desired_capacity_on_demand
+    min_size     = var.min_capacity_on_demand
+    max_size     = var.max_capacity_on_demand
+  }
+
+  instance_types = var.ondemand_instance_types
+
+  capacity_type = "ON_DEMAND"
+  tags = {
+    Name = "eks-on-demand"
+  }
+}
+
+resource "aws_eks_node_group" "spot" {
+  cluster_name    = aws_eks_cluster.main.name
+  node_group_name = "${var.cluster_name}-spot"
+  node_role_arn   = var.node_group_role_arn
+  subnet_ids      = var.private_subnet_ids
+
+  scaling_config {
+    desired_size = var.desired_capacity_spot
+    min_size     = var.min_capacity_spot
+    max_size     = var.max_capacity_spot
+  }
+
+  instance_types = var.spot_instance_types
+
+  capacity_type = "SPOT"
+  tags = {
+    Name = "eks-spot"
+  }
 }
