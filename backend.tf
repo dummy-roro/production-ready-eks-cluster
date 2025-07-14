3918154f7eb496de6e03b1446d40868e1f1de385
Dummy Robot <myatsoeaumg@hotmail.com>
2025-07-14 23:06:26 +0630

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -6,11 +6,11 @@ terraform {
       version = "~> 5.49.0"
     }
   }
-  backend "s3" {
-    bucket         = "dev-aman-tf-bucket"
-    region         = "us-east-1"
-    key            = "eks/terraform.tfstate"
-    use_lockfile   = true
-    encrypt        = true
-  }
+#  backend "s3" {
+#   bucket         = "dev-aman-tf-bucket"
+#    region         = "us-east-1"
+#    key            = "eks/terraform.tfstate"
+#   use_lockfile   = true
+#   encrypt        = true
+# }
 }
